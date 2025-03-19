adf4c99d64e6cf6e4b49db04d3cbceba7186dd26
José Vinicius <vinnyc815@gmail.com>
2025-03-19 00:20:21 -0300

    js,css, otmz

--- js/script.js
+++ js/script.js
@@ -1,23 +1,65 @@
-const imagens = document.querySelectorAll(".bike-magic, .bike-nebula");
+// Ativação do menu
+const links = document.querySelectorAll(".header-botao a");
 
-function galeriaClique(imagem) {
-  imagem.addEventListener("click", galeriaTrocar);
+function ativarLink(link) {
+  const url = location.href;
+  const href = link.href;
+  if (url.includes(href)) {
+    link.classList.add("ativo");
+  }
 }
-imagens.forEach(galeriaClique);
+links.forEach(ativarLink);
 
-function galeriaTrocar(event) {
-  const principal = document.querySelector("#principal");
-  const clicada = event.currentTarget;
+// Ativação orçamento
 
-  // Armazena os atributos da imagem principal
-  const srcTemp = principal.src;
-  const altTemp = principal.getAttribute("alt");
+const parametro = new URLSearchParams(location.search);
 
-  // Troca a imagem principal pela clicada
-  principal.src = clicada.src;
-  principal.setAttribute("alt", clicada.getAttribute("alt"));
+function ativarProduto(parametro) {
+  const elemento = document.getElementById(parametro);
+  if (elemento) {
+    elemento.checked = true;
+  }
+}
 
-  // Define a imagem clicada como a antiga principal
-  clicada.src = srcTemp;
-  clicada.setAttribute("alt", altTemp);
+parametro.forEach(ativarProduto);
+
+//Perguntyas frequentes
+
+const perguntas = document.querySelectorAll(".perguntas button");
+
+function ativarPergunta(event) {
+  const pergunta = event.currentTarget;
+  const controls = pergunta.getAttribute("aria-controls");
+  const resposta = document.getElementById(controls);
+
+  resposta.classList.toggle("ativa");
+  const ativa = resposta.classList.contains("ativa");
+  resposta.setAtribute("aria-expand", ativa);
+}
+
+function eventoPerguntas(pergunta) {
+  pergunta.addEventListener("click", ativarPergunta);
+}
+
+perguntas.forEach(eventoPerguntas);
+
+//Galeria
+
+document.addEventListener("DOMContentLoaded", function () {
+  const imagens = document.querySelectorAll(".nimbus-imagem img");
+
+  imagens.forEach((img, index) => {
+    if (index !== 0) {
+      // Evita adicionar evento na imagem principal
+      img.addEventListener("click", function () {
+        // Troca os src entre a imagem clicada e a primeira imagem (principal)
+        [imagens[0].src, this.src] = [this.src, imagens[0].src];
+      });
+    }
+  });
+});
+
+//animação
+if (window.SimpleAnime) {
+  new SimpleAnime();
 }
